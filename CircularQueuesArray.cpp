ac9721453ac1e21ec3e56f703a85f8c299dd063c
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-06-12 17:48:02 +0700

    void remove

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -41,4 +41,16 @@ public:
         }
         queue_array[REAR] = num;
     }
+
+    void remove()
+    {
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        
+    }
 }
